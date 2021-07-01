921081dddd50d36d5692e1efb22c6842d0c28495
JorgeRos96 <86102864+JorgeRos96@users.noreply.github.com>
2021-07-01 22:04:28 +0200

    Add files via upload

--- joystick.c
+++ joystick.c
@@ -20,16 +20,9 @@ void MX_GPIO_Init(void)
                            CENTER_Pin */
   GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_10|GPIO_PIN_5|GPIO_PIN_14
                           |GPIO_PIN_3;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : LED_Pin */
-  GPIO_InitStruct.Pin = GPIO_PIN_7 | GPIO_PIN_0 | GPIO_PIN_14 ;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI2_IRQn, 0, 0);
